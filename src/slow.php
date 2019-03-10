8a58f2490d26830c890f129775dbe3cdeffb4222
ignacioPL <perezlaborda@gmail.com>
2019-03-10 16:48:17 -0300

    add increment counter

--- src/slow.php
+++ src/slow.php
@@ -7,12 +7,13 @@
     require __DIR__ . '/vendor/autoload.php';
 
     use Brick\Math\BigInteger;
-    
+    $queries = array();
+    parse_str($_SERVER['QUERY_STRING'],$queries);
     $integerA = BigInteger::of('9223372036854775807')->multipliedBy(BigInteger::of('9223372036854775807'));
     $integerB = BigInteger::of('9223372036854775807')->multipliedBy(BigInteger::of('9223372036854775807'));
-
-    for($i = 0; $i < 190; $i++) {
-        $integerC = $integerA->multipliedBy($integerB);
+    //3800
+    for($i = 0; $i < $queries['size']; $i++) {
+        $integerC = $integerA->multipliedBy($integerB)->multipliedBy(BigInteger::of($i));
     }
 
     echo 'done';
